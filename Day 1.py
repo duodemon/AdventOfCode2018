6cd1b6bf18ca92ab31c03d80ba2cda08d384b052
Don Duo <I-am-dond@hotmail.com>
2018-12-29 21:35:36 -0500

    Day 1

--- Day 1.py
+++ Day 1.py
@@ -2,13 +2,27 @@ from utility import convert_input_to_string
 
 
 def part1(puzzle_input):
+    """ sums each line in a text file of frenquencies (signed ints).
+
+    :param puzzle_input: the text file to string
+    :return: the sum of all the frequencies
+    """
     puzzle_input_arr = puzzle_input.split('\n')
     aggregate_frequency = 0
     for frequency in puzzle_input_arr:
         aggregate_frequency += int(frequency)
     return aggregate_frequency
 
+
 def part2(puzzle_input):
+    """ sums each line in a text file of frenquencies (signed ints)
+    and finds the first resulting frequency that appears twice. Wraps around to the start
+    of the file and repeats until found. Using a set instead of list for visited_frequencies
+    decreased runtime by over 100% due to hashing.
+
+    :param puzzle_input: the text file to string
+    :return: the first resulting frequency that appears twice
+    """
     puzzle_input_arr = puzzle_input.split('\n')
     aggregate_frequency = 0
     visited_frequencies = {0}
