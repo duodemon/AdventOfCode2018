e53a00aa3fcec758edad169dc9100123f35a9e35
Don Duo <I-am-dond@hotmail.com>
2018-12-30 22:18:15 -0500

    Day 2 solutions

--- Day 2.py
+++ Day 2.py
@@ -2,11 +2,19 @@ from utility import convert_input_to_string
 
 
 def part1(puzzle_input):
+    """
+    Goes through each line in the input and finds the ID with letters that appear exactly 2 and 3 times, then multiplies
+    them together.
+
+    :param puzzle_input:
+    :return: Numbers of IDs with letters appearing exactly twice
+    """
+
     puzzle_input_arr = puzzle_input.split('\n')
     number_of_ids_with_letter_appearing_twice = 0
     number_of_ids_with_letter_appearing_thrice = 0
     for _id in puzzle_input_arr:
-        number_of_appearances_letter = {}
+        number_of_appearances_letter = {}  # letter: number of appearances in the word
         for letter in _id:
             if letter not in number_of_appearances_letter:
                 number_of_appearances_letter[letter] = 1
@@ -18,21 +26,27 @@ def part1(puzzle_input):
             number_of_ids_with_letter_appearing_thrice += 1
     return number_of_ids_with_letter_appearing_twice * number_of_ids_with_letter_appearing_thrice
 
+
 def part2(puzzle_input):
-    puzzle_input_arr = puzzle_input.split('\n')
-    for (index, _id) in enumerate(puzzle_input_arr):
-        puzzle_input_arr_without_current_id = puzzle_input_arr[:index] + puzzle_input_arr[index + 1:]
-        for _id_to_compare in puzzle_input_arr_without_current_id:
-            number_of_different_letters = 0
-            for letter_index in range(len(_id_to_compare)):
-                if _id[letter_index] != _id_to_compare[letter_index]:
-                    number_of_different_letters += 1
-                    if number_of_different_letters > 1:
-                        break
-            if number_of_different_letters == 1 :
-                for letter_index in range(len(_id_to_compare)):
-                    if _id[letter_index] != _id_to_compare[letter_index]:
-                        return _id[:letter_index] + _id[letter_index+1:]
+    """
+    In the puzzle input, there is only one pair of IDs that differ by exactly one letter. This function finds it and
+    returns the letters in common.
+
+    Goes through every letter in every ID and replaces it with '.' and stores the ID in a set. If the pattern already
+    exists, then that is the duplicate. Return all the other letters.
+
+    :param puzzle_input:
+    :return: Numbers of IDs with letters appearing exactly twice
+    """
+
+    patterns = set()
+    puzzle_input_arr = puzzle_input.strip().split('\n')
+    for _id in puzzle_input_arr:
+        for (index, letter) in enumerate(_id):
+            pattern = _id[:index] + '.' + _id[index + 1:]
+            if pattern in patterns:
+                return pattern.replace('.', '')
+            patterns.add(pattern)
 
 
 if __name__ == "__main__":
