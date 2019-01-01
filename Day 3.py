dc19fffdf7e127c22e035e0986be5a8ebd1a0a60
Don Duo <I-am-dond@hotmail.com>
2019-01-01 17:52:39 -0500

    Day 3 solutions

--- Day 3.py
+++ Day 3.py
@@ -3,9 +3,17 @@ import re
 
 
 def part1(puzzle_input):
-    puzzle_input_arr = puzzle_input.split('\n')
-    visited_squares = {}
-    overlapped_squares = set()
+    """
+    Each line of the the Day 3 text file contains the claim ID, top-left coordinates, height and width. Each claim is a
+    rectangle on the grid. This function reads each line and figures out the number of squares that are overlapped by
+    two or more claims.
+
+    :param puzzle_input: the puzzle input converted to a string
+    :return: the number of squares that are overlapped
+    """
+    puzzle_input_arr = puzzle_input.strip().split('\n')
+    visited_squares = set()  # Coordinates that have been visited once
+    overlapped_squares = set()  # Coordinates that have already been counted into the overlap number
     overlap_number = 0
     for line in puzzle_input_arr:
         line_elements = re.split('#[0-9]+ @|:|,|x', line)
@@ -16,20 +24,26 @@ def part1(puzzle_input):
         for i in range(width):
             for j in range(height):
                 if (i + x_coord, j + y_coord) not in visited_squares:
-                    visited_squares[(i + x_coord, j + y_coord)] = 1
-                else:
-                    visited_squares[(i + x_coord, j + y_coord)] += 1
-                    if (i + x_coord, j + y_coord) not in overlapped_squares:
+                    visited_squares.add((i + x_coord, j + y_coord))
+                elif (i + x_coord, j + y_coord) not in overlapped_squares:
                         overlapped_squares.add((i + x_coord, j + y_coord))
                         overlap_number += 1
     return overlap_number
 
 
 def part2(puzzle_input):
+    """
+    Each line of the the Day 3 text file contains the claim ID, top-left coordinates, height and width. Each claim is a
+    rectangle on the grid. There is only one single claim that is not overlapped by any other claim. This function reads
+    each line in the puzzle input and finds that single claim.
+
+    :param puzzle_input: the puzzle input converted to a string
+    :return: the claim ID that is not overlapped by any other claim
+    """
     puzzle_input_arr = puzzle_input.split('\n')
-    visited_squares = {}
-    all_claims = set()
-    overlapped = set()
+    visited_squares = {}  # (x coordinate, y coordinate): claim ID
+    all_claims = set()  # Every single claim
+    overlapped = set()  # Every single claim that is overlapped
     for line in puzzle_input_arr:
         line_elements = re.split('[#@:,x]', line)
         claim = int(line_elements[1])
