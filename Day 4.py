e7615fe0305c148ffc437ff2cfb44fa6bd40ef4f
Don Duo <I-am-dond@hotmail.com>
2019-01-01 18:38:24 -0500

    Day 4 solutions

--- Day 4.py
+++ Day 4.py
@@ -1,45 +1,34 @@
 from utility import convert_input_to_string
 import re
-import time
 from datetime import datetime
 import collections
 
 
 def part1(puzzle_input):
-    puzzle_input_arr = puzzle_input.split('\n')
-    '''
-      {
-        timestamp: (datetime, action)
-      }
-    '''
-    seconds_to_action = {}
+    """
+    In the text input file, each line shows a date and time, as well as an action (guard begins shift, wakes up, or
+    falls asleep. Part 1 parses the input and finds the guard with the most minutes asleep, and the minute that he is
+    most frequent asleep.
+
+    :param puzzle_input:
+    :return: the guard ID with the most minutes asleep multiplied by the minute that he is most frequent asleep
+    """
+
+    puzzle_input_arr = puzzle_input.strip().split('\n')
+    seconds_to_action = {}  # {timestamp: (datetime, action)}
     for line in puzzle_input_arr:
         m = re.match(r'\[(\d+)-(\d+)-(\d+) (\d+):(\d+)\](.*)', line)
         dt = datetime(1970, int(m.group(2)), int(m.group(3)), int(m.group(4)), int(m.group(5)))
         seconds = dt.timestamp()
         seconds_to_action[seconds] = (dt, m.group(6))
     seconds_sorted = sorted(seconds_to_action.keys())
-
-    '''
-      {
-        'GuardId': sum
-      }
-    '''
-    guard_to_minutes_slept = {}
-
-    '''
-      {
-        'GuardId': [5, 6, 7, 8, 9...24, 30, 31...54, 24, 25, 26, 27, 28]
-      }
-    '''
-    guard_to_minute = {}
-    guard_id = 0
-    sleep_time = None
+    guard_to_minutes_slept = {}  # {guard ID: number of minutes slept}
+    guard_to_minute = {}  # {Guard ID: [5, 6, 7, 8, 9...24, 30, 31...54, 24, 25, 26, 27, 28]}
     for second in seconds_sorted:
         dt, action = seconds_to_action[second]
         if "begins" in action:
             guard_id = int(re.match(r' Guard #(\d+)', action).group(1))
-            if guard_id not in guard_to_minutes_slept:
+            if guard_id not in guard_to_minutes_slept:  # I could use a default dictionary for this
                 guard_to_minutes_slept[guard_id] = 0
                 guard_to_minute[guard_id] = []
         elif "falls" in action:
@@ -48,49 +37,34 @@ def part1(puzzle_input):
             difference_in_minutes = int((dt.timestamp() - sleep_time.timestamp()) // 60)
             guard_to_minutes_slept[guard_id] += difference_in_minutes
             guard_to_minute[guard_id] += [(sleep_time.minute + i) % 60 for i in range(difference_in_minutes)]
-    print(guard_to_minutes_slept)
     guard_with_longest_sleep = max(guard_to_minutes_slept, key=guard_to_minutes_slept.get)
-    print(guard_with_longest_sleep)
     most_common_minute = max(guard_to_minute[guard_with_longest_sleep],
                              key=guard_to_minute[guard_with_longest_sleep].count)
-    print(most_common_minute)
     return guard_with_longest_sleep * most_common_minute
 
 
 def part2(puzzle_input):
+    """
+    In the text input file, each line shows a date and time, as well as an action (guard begins shift, wakes up, or
+    falls asleep. Part 2 parses the input and finds the guard who is the most frequently asleep on the same minute. Same
+    code as part 1 except there is extra dictionary to keep track of who is asleep for each minute and extra
+    functionality to parse it.
+
+    :param puzzle_input:
+    :return: the guard ID who is the most frequently asleep on the same minute multiplied by that minute
+    """
+
     puzzle_input_arr = puzzle_input.split('\n')
-    '''
-      {
-        timestamp: (datetime, action)
-      }
-    '''
-    seconds_to_action = {}
+    seconds_to_action = {}  # {timestamp: (datetime, action)
     for line in puzzle_input_arr:
         m = re.match(r'\[(\d+)-(\d+)-(\d+) (\d+):(\d+)\](.*)', line)
         dt = datetime(1970, int(m.group(2)), int(m.group(3)), int(m.group(4)), int(m.group(5)))
         seconds = dt.timestamp()
         seconds_to_action[seconds] = (dt, m.group(6))
     seconds_sorted = sorted(seconds_to_action.keys())
-
-    '''
-      {
-        'GuardId': sum
-      }
-    '''
-    guard_to_minutes_slept = {}
-
-    '''
-      {
-        'GuardId': [5, 6, 7, 8, 9...24, 30, 31...54, 24, 25, 26, 27, 28]
-      }
-    '''
-    guard_to_minute = {}
-    '''
-      {
-        'minute': [guardIDs]
-      }
-    '''
-    minute_to_guard_slept= {}
+    guard_to_minutes_slept = {}  # {Guard ID: number of minutes slept}
+    guard_to_minute = {}  # {Guard Id: [5, 6, 7, 8, 9...24, 30, 31...54, 24, 25, 26, 27, 28]}
+    minute_to_guard_slept= {}  # {minute: [guard IDs]}
     guard_id = 0
     sleep_time = None
     for second in seconds_sorted:
@@ -110,7 +84,6 @@ def part2(puzzle_input):
                     minute_to_guard_slept[(sleep_time.minute + i) % 60] = [guard_id]
                 else:
                     minute_to_guard_slept[(sleep_time.minute + i) % 60].append(guard_id)
-    print(minute_to_guard_slept)
     most_frequent_number_of_occurrences, sleepiest_guard_id = (0, 0)
     sleepiest_minute = 0
     for minute in minute_to_guard_slept:
@@ -118,10 +91,10 @@ def part2(puzzle_input):
         if c.most_common(1)[0][1] > most_frequent_number_of_occurrences:
             sleepiest_guard_id, most_frequent_number_of_occurrences = c.most_common(1)[0]
             sleepiest_minute = minute
-        print(sleepiest_guard_id, most_frequent_number_of_occurrences  )
     return sleepiest_guard_id * sleepiest_minute
+
 
 if __name__ == "__main__":
     puzzle_input = convert_input_to_string("Day 4.txt")
-    #print(part1(puzzle_input))
+    print(part1(puzzle_input))
     print(part2(puzzle_input))
